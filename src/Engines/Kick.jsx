e02b05d4f8d7ddf006b72016dd6d281773fad6d4
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-12-21 10:58:39 -0800

    kick engine is created

---
+++ src/Engines/Kick.jsx
@@ -0,0 +1,36 @@
+let ctx = AudioContext;
+let osc = OscillatorNode;
+let gain = GainNode;
+export class Kick {
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.tone = 167.1;
+    this.decay = 0.5;
+    this.volume = 1;
+  }
+
+  setup() {
+    this.osc = this.ctx.createOscillator();
+    this.gain = this.ctx.createGain();
+    this.osc.connect(this.gain);
+    this.gain.connect(this.ctx.destination);
+  }
+  trigger(time) {
+    if (this.volume == 0) {
+      return;
+    }
+    this.setup();
+    this.osc.frequency.setValueAtTime(this.tone, time + 0.001);
+    this.gain.gain.linearRampToValueAtTime(this.volume, time + 0.01);
+    this.osc.frequency.exponentialRampToValueAtTime(1, time, this.decay);
+    this.gain.gain.linearRampToValueAtTime(0, time + this.decay + 0.1);
+    thisosc.start(time);
+    this.osc.stop(time + this.decay + 0.1);
+  }
+  setTone = (tone) => {
+    this.tone = tone;
+  };
+  setVolume = (vol) => {
+    this.volume = vol;
+  };
+}
