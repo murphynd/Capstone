e238adf35828486aa4e0c2136c16cc52dd3bcde2
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-12-21 10:39:52 -0800

    Thump printed in Control

---
+++ src/Components/TransportControl.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class TransportControl extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1 style={{ color: "black" }}>ThumP</h1>
+      </div>
+    );
+  }
+}
+export default TransportControl;
